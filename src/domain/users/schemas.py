210956d79138819aaca84a1cd0b7870458967a1c
Yuri Fontella <yurifc4@gmail.com>
2025-12-30 09:37:13 -0300

    fix: change password field type from Optional[int] to Optional[str]

--- src/domain/users/schemas.py
+++ src/domain/users/schemas.py
@@ -45,7 +45,7 @@ class User(Struct, kw_only=True, omit_defaults=True):
     uuid: Optional[UUID] = None
     name: str
     email: str
-    password: Optional[int] = None
+    password: Optional[str] = None
     fingerprint: Optional[int] = None
     role: Optional[UserRole] = None
     status: Optional[bool] = None
